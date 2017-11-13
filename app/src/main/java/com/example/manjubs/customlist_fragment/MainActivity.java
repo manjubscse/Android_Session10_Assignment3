73acfd7a1c40f1ed27b7df0307730075f06a33fa
Manju B S <manjubscse@gmail.com>
2017-11-13 11:24:12 +0530

    Assignment10.3

---
+++ app/src/main/java/com/example/manjubs/customlist_fragment/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.manjubs.customlist_fragment;
+
+import android.app.ListActivity;
+import android.os.Bundle;
+
+
+public class MainActivity extends ListActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+        getFragmentManager().findFragmentById(R.id.fragmentlist);
+    }
+}
